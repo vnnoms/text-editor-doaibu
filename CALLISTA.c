68a05c8beddf2c5cd08c4617074e507654ab1869
vnnoms <callistarhmaputri@gmail.com>
2026-03-24 13:50:25 +0700

    openfiledialog feature

---
+++ CALLISTA.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h> 
+#include "RAMA.h"
+
+#define MAX_BARIS 100
+#define MAX_KARAKTER 255
+extern char bufferTeks[MAX_BARIS][MAX_KARAKTER];
+extern int jumlahBaris;
+
+//Open File Dialog untuk memilih file teks
+void openFileDialog() {
+    OPENFILENAME ofn;
+    char szFile[260] = {0};
+
+    ZeroMemory(&ofn, sizeof(ofn));
+    ofn.lStructSize = sizeof(ofn);
+    ofn.hwndOwner = NULL;
+    ofn.lpstrFile = szFile;
+    ofn.nMaxFile = sizeof(szFile);
+    ofn.lpstrFilter = "Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
+    ofn.nFilterIndex = 1;
+    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+    if (GetOpenFileName(&ofn) == TRUE) {
+        printf("\n[Sistem] File yang dipilih: %s\n", ofn.lpstrFile);
+    } else {
+        printf("\n[Sistem] Batal memilih file.\n");
+    }
+}
