9f4f5c42420ff81bd71f655ca9ac6c5bb0023143
vnnoms <callistarhmaputri@gmail.com>
2026-03-24 22:07:30 +0700

    find feature

--- CALLISTA.c
+++ CALLISTA.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h> 
+#include <ctype.h>
+#include "CONFIG.h"
 #include "RAMA.h"
 
 #define MAX_BARIS 100
@@ -38,5 +40,62 @@ void quitEditor() {
     if (confirm == 'y' || confirm == 'Y') {
         printf("\nTerima kasih telah menggunakan Text Editor Doa Ibu!\n");
         exit(0); 
+    }
 }
+
+void findText(Tab *TT) {
+    char cari[100];
+    printf("\n>> Masukkan kata utuh yang dicari: ");
+    scanf(" %[^\n]", cari);
+
+    int cariLen = strlen(cari);
+    int ketemuTotal = 0;
+    const char *BG_YELLOW = "\033[43;30m"; 
+    const char *RESET = "\033[0m";
+
+    printf("\n--- Hasil Pencarian ---\n");
+
+    for (int i = 0; i < MAX_ROWS; i++) {
+        char *barisSaatIni = TT->text[i]; 
+        char *posisiMatch;
+        int barisIniAdaMatch = 0;
+        char *tempPtr = barisSaatIni;
+        while ((tempPtr = strstr(tempPtr, cari)) != NULL) {
+            int hurufSebelumnyaAman = (tempPtr == barisSaatIni) || !isalnum(*(tempPtr - 1));
+            int hurufSetelahnyaAman = (*(tempPtr + cariLen) == '\0') || (*(tempPtr + cariLen) == '\n') || !isalnum(*(tempPtr + cariLen));
+
+            if (hurufSebelumnyaAman && hurufSetelahnyaAman) {
+                barisIniAdaMatch = 1;
+                ketemuTotal++;
+            }
+            tempPtr += cariLen; 
+        }
+        if (barisIniAdaMatch) {
+            printf("Baris %d: ", i + 1);
+
+            char *cetakPtr = barisSaatIni;
+            while ((posisiMatch = strstr(cetakPtr, cari)) != NULL) {
+                int hurufSebelumnyaAman = (posisiMatch == barisSaatIni) || !isalnum(*(posisiMatch - 1));
+                int hurufSetelahnyaAman = (*(posisiMatch + cariLen) == '\0') || (*(posisiMatch + cariLen) == '\n') || !isalnum(*(posisiMatch + cariLen));
+
+                if (hurufSebelumnyaAman && hurufSetelahnyaAman) {
+                    int panjangTeksBiasa = posisiMatch - cetakPtr;
+                    printf("%.*s", panjangTeksBiasa, cetakPtr);
+                    printf("%s%s%s", BG_YELLOW, cari, RESET);
+                    cetakPtr = posisiMatch + cariLen;
+                } else {
+                    int panjangTeksBiasa = (posisiMatch - cetakPtr) + 1;
+                    printf("%.*s", panjangTeksBiasa, cetakPtr);
+                    cetakPtr = posisiMatch + 1;
+                }
+            }
+            printf("%s\n", cetakPtr);
+        }
+    }
+
+    if (ketemuTotal == 0) {
+        printf("[!] Kata utuh '%s' tidak ditemukan dalam dokumen.\n", cari);
+    } else {
+        printf("\n[i] Selesai! Ditemukan %d kecocokan untuk kata '%s'.\n", ketemuTotal, cari);
+    }
 }
