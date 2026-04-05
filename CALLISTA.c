20c4305bd99507fb1fc5649fb0c48bd1b115d7d1
vnnoms <callistarhmaputri@gmail.com>
2026-04-05 14:13:20 +0700

    addition for case sensitive (find text)

--- CALLISTA.c
+++ CALLISTA.c
@@ -43,13 +43,25 @@ void quitEditor() {
         exit(0); 
     }
 }
+char* cariAbaikanCase(char *teks, char *kataCari) {
+    if (!*kataCari) return teks;
+    for (; *teks; ++teks) {
+        if (tolower(*teks) == tolower(*kataCari)) {
+            char *h = teks, *n = kataCari;
+            while (*h && *n && tolower(*h) == tolower(*n)) {
+                ++h; ++n;
+            }
+            if (!*n) return teks; 
+        }
+    }
+    return NULL; 
+}
 
-void findText(Tab *TT) { //buat dia connected ke fitur replace teks. logika salah satu fitur kalau udah find dan mau replace gak perlu find lagi.
-    //higlight ketika di baris yang akan diedit, dan apabila sudah di edit langsung find kalimat sebelumnya yang telah ditemukan dan telah diganti 
-    //akan tidak terdeteksi lagi oleh mesin pencari. 
+void findText(Tab *TT) {
     char cari[100];
-    printf("\n>> Masukkan kata utuh yang dicari: ");
-    scanf(" %[^\n]", cari);
+    
+    printf(">> Masukkan kata yang dicari: ");
+    scanf("%s", cari); 
 
     int cariLen = strlen(cari);
     int ketemuTotal = 0;
@@ -59,46 +71,25 @@ void findText(Tab *TT) { //buat dia connected ke fitur replace teks. logika sala
     printf("\n--- Hasil Pencarian ---\n");
 
     for (int i = 0; i < MAX_ROWS; i++) {
-        char *barisSaatIni = TT->text[i]; 
+        char *barisSaatIni = TT->text[i];
         char *posisiMatch;
-        int barisIniAdaMatch = 0;
-        char *tempPtr = barisSaatIni;
-        while ((tempPtr = strstr(tempPtr, cari)) != NULL) {
-            int hurufSebelumnyaAman = (tempPtr == barisSaatIni) || !isalnum(*(tempPtr - 1));
-            int hurufSetelahnyaAman = (*(tempPtr + cariLen) == '\0') || (*(tempPtr + cariLen) == '\n') || !isalnum(*(tempPtr + cariLen));
-
-            if (hurufSebelumnyaAman && hurufSetelahnyaAman) {
-                barisIniAdaMatch = 1;
-                ketemuTotal++;
-            }
-            tempPtr += cariLen; 
-        }
-        if (barisIniAdaMatch) {
+        if (cariAbaikanCase(barisSaatIni, cari) != NULL) {
             printf("Baris %d: ", i + 1);
-
             char *cetakPtr = barisSaatIni;
-            while ((posisiMatch = strstr(cetakPtr, cari)) != NULL) {
-                int hurufSebelumnyaAman = (posisiMatch == barisSaatIni) || !isalnum(*(posisiMatch - 1));
-                int hurufSetelahnyaAman = (*(posisiMatch + cariLen) == '\0') || (*(posisiMatch + cariLen) == '\n') || !isalnum(*(posisiMatch + cariLen));
 
-                if (hurufSebelumnyaAman && hurufSetelahnyaAman) {
-                    int panjangTeksBiasa = posisiMatch - cetakPtr;
-                    printf("%.*s", panjangTeksBiasa, cetakPtr);
-                    printf("%s%s%s", BG_YELLOW, cari, RESET);
-                    cetakPtr = posisiMatch + cariLen;
-                } else {
-                    int panjangTeksBiasa = (posisiMatch - cetakPtr) + 1;
-                    printf("%.*s", panjangTeksBiasa, cetakPtr);
-                    cetakPtr = posisiMatch + 1;
-                }
+            while ((posisiMatch = cariAbaikanCase(cetakPtr, cari)) != NULL) {
+                ketemuTotal++; 
+                int panjangTeksBiasa = posisiMatch - cetakPtr;
+                printf("%.*s", panjangTeksBiasa, cetakPtr);
+                printf("%s%.*s%s", BG_YELLOW, cariLen, posisiMatch, RESET);
+                cetakPtr = posisiMatch + cariLen;
             }
             printf("%s\n", cetakPtr);
         }
     }
-
     if (ketemuTotal == 0) {
-        printf("[!] Kata utuh '%s' tidak ditemukan dalam dokumen.\n", cari);
+        printf("[!] Teks '%s' tidak ditemukan.\n", cari);
     } else {
-        printf("\n[i] Selesai! Ditemukan %d kecocokan untuk kata '%s'.\n", ketemuTotal, cari);
+        printf("\n[i] Selesai! Ditemukan %d kecocokan.\n", ketemuTotal);
     }
 }
